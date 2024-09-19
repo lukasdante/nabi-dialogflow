c5f549bddc7265e751fbcea054623ed5cf4dd4d4
lukasdante <johnlouis.dante@gmail.com>
2024-09-19 22:45:36 +0800

    feat: 	erminate session for terminating infinite conversational turns

--- scripts/main.py
+++ scripts/main.py
@@ -26,12 +26,14 @@ def record_and_transcribe():
         # play the audio output
         play_mp3("synthesize-text-audio.mp3")
 
-        print(parameters)
+        return parameters
 
     except Exception as e:
         print(e)
 
 if __name__ == "__main__":
+    SESSION_ID = uuid.uuid4() 
+
     load_dotenv()
     STT_API_KEY = os.getenv('STT_API_KEY')
     TTS_API_KEY = os.getenv('TTS_API_KEY')
@@ -40,12 +42,11 @@ if __name__ == "__main__":
     AGENT_ID = os.getenv("DFCX_AGENT_ID")
     service_account_path = os.getenv("DFCX_SERVICE_ACCOUNT")
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_path
-    
-    new_session_id=False
-    
-    if new_session_id:
-        SESSION_ID = uuid.uuid4() 
-    else:
-        SESSION_ID = "test-session"
 
-    record_and_transcribe()
+    while not terminate:
+        print(terminate)
+        parameters = record_and_transcribe()
+        print(parameters['terminate'])
+
+        if parameters['terminate'] == "True":
+            terminate = True
