ac059be143f2eb7d403264da11e310002a95464c
lukasdante <johnlouis.dante@gmail.com>
2024-09-19 15:45:32 +0800

    Temp fix for parsing parameters

--- scripts/detect_intent.py
+++ scripts/detect_intent.py
@@ -4,15 +4,17 @@ from dotenv import load_dotenv
 from google.cloud.dialogflowcx_v3beta1.services.agents import AgentsClient
 from google.cloud.dialogflowcx_v3beta1.services.sessions import SessionsClient
 from google.cloud.dialogflowcx_v3beta1.types import session
+from google.protobuf.json_format import MessageToDict
 
-def detect_intent_text(text, project_id, location_id, agent_id):
+def detect_intent_text(text, project_id, location_id, agent_id, session_id=None):
     """Returns the result of detect intent with texts as inputs.
 
     Using the same `session_id` between requests allows continuation
     of the conversation."""
         
     agent = f"projects/{project_id}/locations/{location_id}/agents/{agent_id}"
-    session_id = uuid.uuid4()
+    if not session_id:
+        session_id = uuid.uuid4()
     language_code = "en-us"
 
     session_path = f"{agent}/sessions/{session_id}"
@@ -22,7 +24,6 @@ def detect_intent_text(text, project_id, location_id, agent_id):
     api_endpoint = f"{location_id}-dialogflow.googleapis.com:443"
     client_options = {"api_endpoint": api_endpoint}
     session_client = SessionsClient(client_options=client_options)
-
     text_input = session.TextInput(text=text)
     query_input = session.QueryInput(text=text_input, language_code=language_code)
     request = session.DetectIntentRequest(
@@ -34,13 +35,21 @@ def detect_intent_text(text, project_id, location_id, agent_id):
     response_messages = [
         " ".join(msg.text.text) for msg in response.query_result.response_messages
     ]
+
+    parameters = MessageToDict(response._pb)
+    
     print(f"Response text: {' '.join(response_messages)}\n")
 
-    return ' '.join(response_messages)
+    response_output = ' '.join(response_messages)
+
+    return (response_output, parameters['queryResult']['parameters'])
 
 if __name__ == "__main__":
     load_dotenv()
     PROJECT_ID = os.getenv("DFCX_PROJECT_ID")
     LOCATION_ID = os.getenv("DFCX_LOCATION_ID")
     AGENT_ID = os.getenv("DFCX_AGENT_ID")
-    detect_intent_text("Hello", project_id=PROJECT_ID, location_id=LOCATION_ID, agent_id=AGENT_ID)
+    service_account_path = os.getenv("DFCX_SERVICE_ACCOUNT")
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_path
+
+    detect_intent_text("Put the red box in the mat", project_id=PROJECT_ID, location_id=LOCATION_ID, agent_id=AGENT_ID, session_id="test-session")
--- scripts/main.py
+++ scripts/main.py
@@ -1,27 +1,32 @@
 from recorder import record_audio
 from stt_request import transcribe_audio
-from dfscript import detect_intent_text
+from detect_intent import detect_intent_text
 from tts_request import init_tts, decode_tts_output, json_write
 from play_sound import play_mp3
 from dotenv import load_dotenv
 import os
+import uuid
 
 def record_and_transcribe():
     try:
         # record audio
         record_audio(threshold=1500)
+
         # transcribe the input audio
-        print("Transcribing audio...")
         transcribed_audio = transcribe_audio(STT_API_KEY, "output.wav")
+
         # detect intent from the transcribed audio
-        response = detect_intent_text(transcribed_audio, PROJECT_ID, LOCATION_ID, AGENT_ID)
+        response, parameters = detect_intent_text(transcribed_audio, PROJECT_ID, LOCATION_ID, AGENT_ID, SESSION_ID)
+        
         # decode the response to an audio output
         data = json_write(response)
         api_response = init_tts(data)
-
         decode_tts_output(api_response, "synthesize-text-audio.mp3")
-        # TODO: automatically run the audio output
+        
+        # play the audio output
         play_mp3("synthesize-text-audio.mp3")
+
+        print(parameters)
 
     except Exception as e:
         print(e)
@@ -33,4 +38,14 @@ if __name__ == "__main__":
     PROJECT_ID = os.getenv("DFCX_PROJECT_ID")
     LOCATION_ID = os.getenv("DFCX_LOCATION_ID")
     AGENT_ID = os.getenv("DFCX_AGENT_ID")
+    service_account_path = os.getenv("DFCX_SERVICE_ACCOUNT")
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_path
+    
+    new_session_id=False
+    
+    if new_session_id:
+        SESSION_ID = uuid.uuid4() 
+    else:
+        SESSION_ID = "test-session"
+
     record_and_transcribe()
